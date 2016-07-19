7ed44ff096d32bb13c1a060b83acb6419b49ea97
maki077 <lqx077@163.com>
2016-07-19 17:07:11 +0800

    侧滑xml添加

---
+++ app/src/main/java/com/maki/project/view/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.maki.project.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.maki.project.R;
+
+/**
+ * Administrator on 2016/7/19.
+ */
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
