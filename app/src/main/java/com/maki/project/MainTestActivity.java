b84bc07fa861a6da61aa1dad7a840b40bf5c614a
maki077 <lqx077@163.com>
2016-07-19 11:40:53 +0800

    初次上传 测试MD colorPrimary colorPrimaryDark colorAccent

---
+++ app/src/main/java/com/maki/project/MainTestActivity.java
@@ -0,0 +1,16 @@
+package com.maki.project;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Administrator on 2016/7/19.
+ */
+public class MainTestActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_test);
+    }
+}
